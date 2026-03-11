5988fab23eddcb356db633a002accd8ab6deaf97
mursalniazi442 <mursalniazi119@gmail.com>
2026-03-11 13:21:48 -0400

    Organized JavaScript event sections

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// JavaScript event handling for lab 7 // Mouse Events
+// JavaScript event handling for lab 7 
+
+//Mouse Events
+//Keyboard Events
+//Form Events
+//Ffocuse and Blur Events
+//Event Delegation
+
+
+// Mouse Events
 
 const hoverButton = document.getElementById("hoverButton");
 
